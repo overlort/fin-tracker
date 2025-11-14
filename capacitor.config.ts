042a23dbbad1e48f61b8d7a659cb5af83682f114
Konstantin <myrz47@mail.ru>
2025-11-14 11:47:47 +0300

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.fintracker',
+  appName: 'fin-tracker',
+  webDir: 'build'
+};
+
+export default config;
