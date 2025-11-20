b46e29f66033ad19b04aa331ae762d253aed514c
Konstantin <myrz47@mail.ru>
2025-11-20 11:46:39 +0300

    Build error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,83 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  base: "/fin-tracker/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Учет Финансов",
+        short_name: "FiT",
+        description: "Приложение для учета личных финансов",
+        start_url: ".",
+        scope: "/",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#282d32",
+        theme_color: "#282d32",
+        icons: [
+          {
+            src: 'favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'icons/icon-180x180.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     outDir: "./build",
   },
   resolve: {
     alias: {
-      "@": "/src"
+      "@": path.resolve(__dirname, "./src")
     }
   }
 });
