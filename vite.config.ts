563ba8397aff82b2d03a421a91cb4b0e1c97324d
Konstantin <myrz47@mail.ru>
2025-11-27 12:40:27 +0300

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
-  base: "/fin-tracker/",
+  base: "/",
   plugins: [
     react(),
     VitePWA({
